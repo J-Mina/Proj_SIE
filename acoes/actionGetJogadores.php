5f2c86009223617197525c245fa6b15b421e72a5
João Mina <78810007+J-Mina@users.noreply.github.com>
2023-01-19 21:45:18 +0000

    Tabela Jogadores

---
+++ acoes/actionGetJogadores.php
@@ -0,0 +1,48 @@
+
+<?php
+
+$conn = pg_connect("host=db.fe.up.pt dbname=sie2250 user=sie2250 password=NIQHxBGU");
+$query = "set schema 'sundayleague'";	
+pg_exec($conn, $query);
+
+if (!$conn) {
+    die("Error connecting to the database");
+}
+
+$query = "SELECT * FROM jogador,posicao,equipa where jogador.id_posicao = posicao.id_posicao and jogador.id_equipa=equipa.id_equipa";
+$result = pg_query($conn, $query);
+
+
+
+echo "<table class=\"classificacao\" border = 0 >";
+echo      "<tr>";
+echo          "<th>Nome</th><th>Idade</th><th>Altura</th><th>Posição</th><th>Equipa</th>";
+echo      "</tr>";
+
+$numRows = pg_numrows($result);
+$i=0;
+while($i< $numRows){
+
+
+    $row = pg_fetch_row($result, $i);
+
+    $nome = $row[1];
+    $idade = $row[2];
+    $altura = $row[3];
+    $posicao = $row[10];
+    $equipa = $row[12];
+
+    echo "<tr>";
+    echo    "<th>".$nome."</th><th>".$idade."</th><th>".$altura."</th><th>".$posicao."</th><th>".$equipa."</th>";
+    echo "</tr>";
+
+    $i++;
+}
+
+echo "</table>";
+
+pg_close($conn);
+    
+
+?> 
+
--- paginas/jogadores.php
+++ paginas/jogadores.php
@@ -53,6 +53,8 @@
           <h2 id="subtitulos">Pesquisa os nossos Jogadores</h2>
 
           <div> <?php include 'formPesquisaJogadores.php' ?></div>
+          <br>
+          <div class = "pagina_classificacao"><div class ="classificacao"> <?php include '../acoes/actionGetJogadores.php' ?></div></div>
     </div>
 
      

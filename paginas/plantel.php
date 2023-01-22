cac828c405f06544dee6be6cef1ef38dfba8ec37
MiguelCorreia6 <78810277+MiguelCorreia6@users.noreply.github.com>
2023-01-22 20:13:39 +0000

    plantel

--- paginas/plantel.php
+++ paginas/plantel.php
@@ -1,7 +1,21 @@
-<?php include 'inc/header.php' ?>
+<?php include 'inc/header.php';
+      include "../baseDados/connect.php";
 
 
-     <h2 id="subtitulos2">Plantel equipa Chaminés às Costas</h2>
+          $query="SELECT nome_jogador, equipa.nome as nome_equipa,logo_equipa,id_posicao
+          FROM jogador
+          JOIN equipa ON jogador.id_equipa = equipa.id_equipa
+          WHERE equipa.id_equipa=7";
+
+          $result = pg_query($conn,$query);
+          $player = pg_fetch_assoc($result);
+
+          $numRows = pg_numrows($result);
+          $i=0;
+
+ ?>
+
+     <h2 id="subtitulos2">Plantel equipa <?php echo $player['nome_equipa']; ?></h2>
 
 
 
@@ -11,77 +25,82 @@
                <a href="equipas.phps"><button><i class="fa fa-arrow-circle-left" aria-hidden="true"></i> Voltar</button></a>
           </div>
 
-          <img src="../imagens/chamines.png">
+          <img src=<?php echo $player['logo_equipa']; ?>>
 
-
-
-          <table class="pagina_plantel">
+          <table>
                <tr>
-                    <th>Guarda-Redes</th>
+               <th>Guarda-Redes</th>
                </tr>
-               <tr>
-                    <td><a href="jogador.php">Iker Casillas</a></td>
-               </tr>
-               <tr>
-                    <td>Roberto</td>
-               </tr>
+               
+               <?php
+                    $resultgr = pg_query($conn,$query);
+                    while($row = pg_fetch_assoc($resultgr)) {
+                         if ($row['id_posicao'] ==4) {
+               ?>
+                    <tr>
+                         <td><?php echo $row['nome_jogador']; ?></td>
+                    </tr>
+               <?php
+                         }
+                    }
+               ?>
           </table>
 
-
-          <table class="pagina_plantel">
+          <table>
                <tr>
-                    <th>Defesas</th>
+               <th>Defesa</th>
                </tr>
-               <tr>
-                    <td>Sergio Ramos</td>
-               </tr>
-               <tr>
-                    <td>Maldini</td>
-               </tr>
-               <tr>
-                    <td>Daniel Alves</td>
-               </tr>
-               <tr>
-                    <td>Zaidu</td>
-               </tr>
+               
+               <?php
+                         $resultdefesa = pg_query($conn,$query);
+                    while($row = pg_fetch_assoc($resultdefesa)) {
+                         if ($row['id_posicao'] ==2) {
+               ?>
+                    <tr>
+                         <td><?php echo $row['nome_jogador']; ?></td>
+                    </tr>
+               <?php
+                         }
+                    }
+               ?>
           </table>
 
-          <table class="pagina_plantel">
+          <table>
                <tr>
-                    <th>Médios</th>
+               <th>Médio</th>
                </tr>
-               <tr>
-                    <td>Modric</td>
-               </tr>
-               <tr>
-                    <td>Kroos</td>
-               </tr>
-               <tr>
-                    <td>Iniesta</td>
-               </tr>
-               <tr>
-                    <td>Taarabt</td>
-               </tr>
+               
+               <?php
+                         $resultmedio = pg_query($conn,$query);
+                    while($row = pg_fetch_assoc($resultmedio)) {
+                         if ($row['id_posicao'] ==3) {
+               ?>
+                    <tr>
+                         <td><?php echo $row['nome_jogador']; ?></td>
+                    </tr>
+               <?php
+                         }
+                    }
+               ?>
           </table>
 
-
-
-          <table class="pagina_plantel">
+          <table>
                <tr>
-                    <th>Avançados</th>
+               <th>Avançado</th>
                </tr>
-               <tr>
-                    <td>Cristano Ronaldo</td>
-               </tr>
-               <tr>
-                    <td>Messi</td>
-               </tr>
-               <tr>
-                    <td>Neymar</td>
-               </tr>
-               <tr>
-                    <td>Paulinho</td>
-               </tr>
+               
+               <?php
+                         $resultavancado = pg_query($conn,$query);
+                    while($row = pg_fetch_assoc($resultavancado)) {
+                         if ($row['id_posicao'] ==1) {
+               ?>
+                    <tr>
+                         <td><?php echo $row['nome_jogador']; ?></td>
+                    </tr>
+               <?php
+                         }
+                    }
+               ?>
           </table>
 
 

cfd6a9b92bd3dd7a43a21286470e248fdfe91e54
MiguelCorreia6 <78810277+MiguelCorreia6@users.noreply.github.com>
2023-01-23 11:00:37 +0000

    registo

--- paginas/formRegisto.php
+++ paginas/formRegisto.php
@@ -3,20 +3,33 @@
 </head>
 
 <html>
-        <form class = "formRegisto" action="../acoes/geral/actionRegisto.php" method="post">
-            <label for="name">Nome:</label>
-            <input type="text" id="nome" name="nome">
-            <br>
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username">
-            <br>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password">
-            <br>
-            <label for="conf_password">Confirmar Password:</label>
-            <input type="password" id="conf_password" name="conf_password">
-            <br>
-            <input type="submit" value="Registar">
-        </form>
+
+        <div class="formRegisto">
+            <form action="../acoes/geral/actionRegisto.php" method="post">
+                <table>
+                    <tr>
+                        <td><label for="name">Nome:</label></td>
+                        <td><input type="text" id="nome" name="nome"></td>
+                    </tr>
+
+                    <tr>
+                        <td><label for="username">Username:</label></td>
+                        <td><input type="text" id="username" name="username"></td>
+                    </tr>
+
+                    <tr>
+                        <td><label for="password">Password:</label></td>
+                        <td><input type="password" id="password" name="password"></td>
+                    </tr>
+
+                    <tr>
+                        <td><label for="conf_password">Confirmar Password:</label></td>
+                        <td><input type="password" id="conf_password" name="conf_password"></td>
+                    </tr>
+                </table>
+            
+                <input type="submit" value="Registar">
+            </form>
+        </div>
 
 </html>

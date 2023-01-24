59262cbb56f9a79a25460781edc2035317c4e241
MiguelCorreia6 <78810277+MiguelCorreia6@users.noreply.github.com>
2023-01-24 14:29:37 +0000

    pesquisaEquipas

--- paginas/equipa/formPesquisaEquipas.php
+++ paginas/equipa/formPesquisaEquipas.php
@@ -2,25 +2,40 @@
     <link rel="stylesheet" type="text/css" href="../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+<?php  include "../../baseDados/connect.php";?>
 
 <html>
         <form class = "formPesquisaJogadores" action="equipas.php" method="post">
             <input type="text" id="nomeEquipa" name="nomeEquipa" placeholder ="Nome da Equipa">
             &emsp;
-            <select name="localidade" id="localidade">
+            <!--  <select name="localidade" id="localidade">
                 <option value=""> Localidade</option>
                 <option value="Porto"> Porto</option>
                 <option value="Lisboa"> Lisboa</option>
                 <option value="Braga"> Braga</option>
+            </select>-->
+            <select name="localidade" id="localidade">
+                <option value=""> Localidade</option>
+                <?php
+                    $query = "SELECT nome_cidade FROM cidade ORDER BY nome_cidade";
+                    $result = pg_query($conn, $query);
+                    while($row = pg_fetch_assoc($result)) {
+                        echo "<option value='".$row['nome_cidade']."'>".$row['nome_cidade']."</option>";
+                    }
+                ?>
             </select>
             &emsp;
             <input type="text" id="treinador" name="treinador" placeholder="Nome Treinador">
             &emsp;
             <select name="fundado" id="fundado">
-                <option value=""> Fundado em ...</option>
-                <option value="1973"> 1973</option>
-                <option value="1982"> 1982</option>
-                <option value="1945"> 1945</option>
+                <option value=""> Ano de fundação</option>
+                <?php
+                    $query = "SELECT data_fundacao FROM equipa ORDER BY data_fundacao";
+                    $result = pg_query($conn, $query);
+                    while($row = pg_fetch_assoc($result)) {
+                        echo "<option value='".$row['data_fundacao']."'>".$row['data_fundacao']."</option>";
+                    }
+                ?>
             </select>
             &emsp;
             <input type="submit" value="Search">
